fa09cbcaf632b73a061f7056b4ac55e5d506e63c
Horacio G. de Oro <hgdeoro@gmail.com>
2011-04-15 01:54:33 -0300

    Implemented analogRead() and utility functions readCmd(), stringToInt()

--- src/arduino_proxy/proxy.py
+++ src/arduino_proxy/proxy.py
@@ -11,9 +11,39 @@ class ArduinoProxy(object):
     def __arduino_setup(self):
         # FIXME: implementar!
         return """
+        char lastCmd[128];
+        
         void setup() {
             // Serial.xxxxxxx()
-            String lastCmd = "";
+            
+        }
+        
+        void readCmd() {
+            int incomingByte = 0xff;
+            int pos = 0;
+            lastCmd[0] = 0;
+            
+            while (Serial.available() > 0 && pos < 128) {
+                // read the incoming byte:
+                lastCmd[pos] = Serial.read();
+                if(lastCmd[pos] == 0) {
+                    break;
+                }
+                pos++;
+            }
+        }
+
+        void sendOkResponse() {
+            // Serial.writeXxxxx("OK")
+        }
+        
+        int stringToInt(String str) {
+            char buff[str.length() + 1];
+            str.toCharArray(buff, (str.length() + 1));
+            return atoi(buff); 
+        }
+        void sendIntResponse(int value) {
+            // Serial.writeXxxxx(value, INTEGER)
         }
         """
 
@@ -26,6 +56,9 @@ class ArduinoProxy(object):
     
     def sendCmd(self, cmd):
         # FIXME: implementar
+        
+        # Serial.send("%s\x00" % cmd)
+        
         pass
     
     # Digital I/O
@@ -40,11 +73,13 @@ class ArduinoProxy(object):
                 String pin = cmd.substring(index_of_pin, index_of_mode);
                 String mode = cmd.substring(index_of_mode);
                 if(mode.equals("%(INPUT)s")) {
-                    pinMode(int(pin), INPUT);
+                    pinMode(stringToInt(pin), INPUT);
+                    sendOkResponse();
                     return;
                 }
                 if(mode.equals("%(OUTPUT)s")) {
-                    pinMode(int(pin), OUTPUT);
+                    pinMode(stringToInt(pin), OUTPUT);
+                    sendOkResponse();
                     return;
                 }
             }
@@ -80,11 +115,29 @@ class ArduinoProxy(object):
     def analogReference(self):
         pass
     
-    def analogRead(self):
+    def __arduino__analogRead(self):
+        return """
+            void _analogRead(String cmd) {
+                if(!cmd.startsWith("%(method)s")) {
+                    return;
+                }
+                int index_of_pin = cmd.indexOf(' ');
+                String pin = cmd.substring(index_of_pin);
+                int value = analogRead(stringToInt(pin));
+                sendIntResponse(value);
+                return;
+            }
+        """ % {
+        'method': 'analogRead', 
+    }
+    
+    def analogRead(self, pin):
         """
         * map input voltages between 0 and 5 volts into integer values between 0 and 1023.
         """
-        pass
+        cmd = "_analogRead %d" % (pin)
+        ret = self.sendCmd(cmd)
+        return ret
     
     def analogWrite(self):
         pass
@@ -100,9 +153,10 @@ if __name__ == '__main__':
         print method()
 
     print "void loop() {"
-    print "    String cmd = Serial.readXxxxx()"
+    print "    readCmd();"
     for method_name in methods:
         if method_name in ['setup']:
             continue
-        print "    " + method_name + "(cmd)"
+        print "    //" + method_name + "(cmd);"
+        print "    " + method_name + "(String(lastCmd));"
     print "}"
