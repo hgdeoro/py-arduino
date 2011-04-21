719e2b464c65983e083e0831526193bf0ea6553f
Horacio G. de Oro <hgdeoro@gmail.com>
2011-04-21 13:11:29 -0300

    Changed analog_read.py so can be called as a script. Implemented utility method for this scripts.

--- src/arduino_proxy/tests/analog_read.py
+++ src/arduino_proxy/tests/analog_read.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 ##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 ##    Py-Arduino-Proxy - Access your Arduino from Python
 ##    Copyright (C) 2011 - Horacio Guillermo de Oro <hgdeoro@gmail.com>
@@ -17,42 +18,46 @@
 ##    along with Py-Arduino-Proxy; see the file LICENSE.txt.
 ##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-import logging
+import os
 import sys
 
-from arduino_proxy import ArduinoProxy
+# Setup PYTHONPATH
+SRC_DIR = os.path.split(os.path.realpath(__file__))[0] # SRC_DIR/arduino_proxy/tests
+SRC_DIR = os.path.split(SRC_DIR)[0] # SRC_DIR/arduino_proxy
+SRC_DIR = os.path.split(SRC_DIR)[0] # SRC_DIR
+sys.path.append(os.path.abspath(SRC_DIR))
+
+from arduino_proxy.tests import default_main
 
 def default_callback(value):
     print value
 
+def args_validator(parser, options, args): # pylint: disable=W0613
+    if len(args) != 2:
+        parser.error("must specified two argument: serial device and analog port")
+
+def add_options_callback(parser):
+    parser.add_option("--loop",
+        action="store_true", dest="loop", default=False,
+        help="Keep reading and printing the values.")
+
 def main(callback):
-    argv = list(sys.argv)
-    if "--debug" in argv:
-        logging.basicConfig(level=logging.DEBUG)
-        argv.remove("--debug")
-    else:
-        logging.basicConfig(level=logging.ERROR)
+    options, args, proxy = default_main(quiet=True, optparse_usage=\
+        "usage: %prog [options] serial_device analog_port", args_validator=args_validator,
+        add_options_callback=add_options_callback)
     
-    if "--loop" in argv:
-        do_loop = True
-        argv.remove("--loop")
-    else:
-        do_loop = False
+    analog_port = int(args[1])
     
-    try:
-        serial = argv[1]
-        analog_port = int(argv[2])
-    except (IndexError, ValueError, ):
-        print "Use: %s <SERIAL_PORT> <ANALOG_PORT_OF_ARDUINO>" % argv[0]
-        sys.exit(1)
-    
-    proxy = ArduinoProxy(serial, 9600)
     try:
         while True:
             value = proxy.analogRead(analog_port)
             callback(value)
-            if not do_loop:
+            if not options.loop:
                 break
+    except KeyboardInterrupt:
+        print ""
+    except Exception:
+        raise
     finally:
         proxy.close()
 
