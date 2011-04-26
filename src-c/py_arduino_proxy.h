3f3b00d64afc7867011aa1467add2a41791cf930
Horacio G. de Oro <hgdeoro@gmail.com>
2011-04-26 20:29:51 -0300

    Added '#ifdef __cplusplus' to .h file

--- src-c/py_arduino_proxy.h
+++ src-c/py_arduino_proxy.h
@@ -1,5 +1,17 @@
+#ifndef	PY_ARDUINO_PROXY_H
+#define PY_ARDUINO_PROXY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // typedef for proxied functions.
 
 typedef void (* proxied_function_ptr) ();
 
+#ifdef  __cplusplus
+}
+#endif
+
+#endif
