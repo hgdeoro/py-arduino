d0203cde8a3405a1e8d5d5acbabd542b6c784b6e
Horacio G. de Oro <hgdeoro@gmail.com>
2011-05-03 18:01:24 -0300

    EMPTY COMMIT. Changed tabs to spaces, no real code changed.

--- src/arduino_proxy/ui/main.py
+++ src/arduino_proxy/ui/main.py
@@ -16,33 +16,33 @@ from arduino_proxy.ui.main_ui import *
 from arduino_proxy.tests import default_main
 
 class Subclass(Ui_MainWindow):
-	
-	def __init__(self, qMainWindow, options, args, proxy):
-		self.qMainWindow = qMainWindow
-		self.options = options
-		self.args = args
-		self.proxy = proxy
-		Ui_MainWindow.setupUi(self, self.qMainWindow)
-		
-		for pin_mode_attr in [ self.pinMode13, self.pinMode12,
-				self.pinMode11, self.pinMode10, self.pinMode9,
-				self.pinMode8]:
-			self.qMainWindow.connect(pin_mode_attr,
-				QtCore.SIGNAL('clicked()'), self.pinModeClicked)
-		
-		#self.led13.setPixmap(QtGui.QPixmap(":/images/led-off.png"))
-		#self.qMainWindow.connect(self.led13,
-		#	QtCore.SIGNAL('clicked()'), self.setPixmap123)
-	
-	#def setPixmap123(self):
-	#	self.led13.setPixmap(QtGui.QPixmap(":/images/led-on.png"))
-	
-	def pinModeClicked(self):
-		sender = self.qMainWindow.sender()
-		if sender.text() == 'I':
-			sender.setText('O')
-		else:
-			sender.setText('I')
+    
+    def __init__(self, qMainWindow, options, args, proxy):
+        self.qMainWindow = qMainWindow
+        self.options = options
+        self.args = args
+        self.proxy = proxy
+        Ui_MainWindow.setupUi(self, self.qMainWindow)
+        
+        for pin_mode_attr in [ self.pinMode13, self.pinMode12,
+                self.pinMode11, self.pinMode10, self.pinMode9,
+                self.pinMode8]:
+            self.qMainWindow.connect(pin_mode_attr,
+                QtCore.SIGNAL('clicked()'), self.pinModeClicked)
+        
+        #self.led13.setPixmap(QtGui.QPixmap(":/images/led-off.png"))
+        #self.qMainWindow.connect(self.led13,
+        #    QtCore.SIGNAL('clicked()'), self.setPixmap123)
+    
+    #def setPixmap123(self):
+    #    self.led13.setPixmap(QtGui.QPixmap(":/images/led-on.png"))
+    
+    def pinModeClicked(self):
+        sender = self.qMainWindow.sender()
+        if sender.text() == 'I':
+            sender.setText('O')
+        else:
+            sender.setText('I')
 
 def default_args_validator(parser, options, args): # pylint: disable=W0613
     if len(args) <1:
@@ -57,4 +57,4 @@ def main():
     sys.exit(app.exec_())
     
 if __name__ == '__main__':
-	main()
+    main()
