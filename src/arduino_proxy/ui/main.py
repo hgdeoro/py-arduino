3fdd559bf0af6ae59ea099f3a18e2af76a4c13a4
Horacio G. de Oro <hgdeoro@gmail.com>
2011-05-03 18:00:34 -0300

    Added proxy instance con ui.

--- src/arduino_proxy/ui/main.py
+++ src/arduino_proxy/ui/main.py
@@ -11,12 +11,17 @@ SRC_DIR = os.path.split(SRC_DIR)[0] # SRC_DIR/arduino_proxy
 SRC_DIR = os.path.split(SRC_DIR)[0] # SRC_DIR
 sys.path.append(os.path.abspath(SRC_DIR))
 
+from arduino_proxy import ArduinoProxy
 from arduino_proxy.ui.main_ui import *
+from arduino_proxy.tests import default_main
 
 class Subclass(Ui_MainWindow):
 	
-	def __init__(self, qMainWindow):
+	def __init__(self, qMainWindow, options, args, proxy):
 		self.qMainWindow = qMainWindow
+		self.options = options
+		self.args = args
+		self.proxy = proxy
 		Ui_MainWindow.setupUi(self, self.qMainWindow)
 		
 		for pin_mode_attr in [ self.pinMode13, self.pinMode12,
@@ -39,12 +44,17 @@ class Subclass(Ui_MainWindow):
 		else:
 			sender.setText('I')
 
-def main():
-	app = QtGui.QApplication(sys.argv)
-	MainWindow = QtGui.QMainWindow()
-	ui = Subclass(MainWindow)
-	MainWindow.show()
-	sys.exit(app.exec_())
+def default_args_validator(parser, options, args): # pylint: disable=W0613
+    if len(args) <1:
+        parser.error("You should specify the serial device.")
 
+def main():
+    options, args, proxy = default_main(args_validator=default_args_validator)
+    app = QtGui.QApplication(args[1:])
+    MainWindow = QtGui.QMainWindow()
+    ui = Subclass(MainWindow, options, args, proxy)
+    MainWindow.show()
+    sys.exit(app.exec_())
+    
 if __name__ == '__main__':
 	main()
