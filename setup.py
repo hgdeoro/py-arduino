bc95148dbca50430f2fcd3eb04ac8f8ec86a4096
Horacio G. de Oro <hgdeoro@gmail.com>
2011-06-08 16:24:25 -0300

    Web-UI: tried to install htmls, css, images, etc. using 'data_files' of distuilt, without luck...

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
+import os
+
 from distutils.core import setup
+
+#def static_files_list():
+#    all_files = []
+#    directory = os.path.join(os.path.split(__file__)[0], 'src', 'arduino_proxy', 'webui', 'static')
+#    for root, dirs, files in os.walk(directory):
+#        for a_file in files:
+#            all_files.append(os.path.join(root, a_file))
+#            #all_files.append('/'.join(os.path.join(root, a_file).split('/')[2:]))
+#    return all_files
 
 name = "PyArduinoProxy"
 version = "0.0.1-alpha"
@@ -6,7 +17,11 @@ description = 'Communicate with Arduino from Python, a web page and JavaScript'
 author="Horacio G. de Oro"
 author_email = "hgdeoror@gmail.com"
 url = "http://pyarduinoproxy.blogspot.com/"
+
+packages = ['arduino_proxy', 'arduino_proxy.ui', 'arduino_proxy.tests', 'arduino_proxy.webui']
 package_dir = {'':'src', }
+package_data = {'': ['webui/static/*'], }
+
 classifiers = [ # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     "Development Status :: 3 - Alpha",
     "Environment :: Console", 
@@ -22,6 +37,9 @@ classifiers = [ # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     "Topic :: Utilities",
 ]
 
+#data_files_static = static_files_list()
+#assert data_files_static # print data_files_static
+
 setup(
     name=name,
     version=version,
@@ -30,8 +48,13 @@ setup(
     author_email=author_email,
     url=url,
     package_dir=package_dir, 
-    packages=['arduino_proxy', 'arduino_proxy.ui', 'arduino_proxy.tests'],
+    packages=packages, 
     classifiers=classifiers, 
+    #    data_files=[
+    #        #('arduino_proxy', static_files_list()),
+    #        ('', data_files_static),
+    #    ]
+    #    package_data=package_data, 
 )
 
 ##import os
