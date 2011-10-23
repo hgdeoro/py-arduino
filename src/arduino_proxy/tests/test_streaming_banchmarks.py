cf82bae1670ee4922d1ec5ec2c4d3df1b8163eb0
Horacio G. de Oro <hgdeoro@gmail.com>
2011-10-22 22:57:57 -0300

    Added test for benchmarking streaming vs non-streaming analog reads.

---
+++ src/arduino_proxy/tests/test_streaming_banchmarks.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+##    Py-Arduino-Proxy - Access your Arduino from Python
+##    Copyright (C) 2011 - Horacio Guillermo de Oro <hgdeoro@gmail.com>
+##
+##    This file is part of Py-Arduino-Proxy.
+##
+##    Py-Arduino-Proxy is free software; you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation version 2.
+##
+##    Py-Arduino-Proxy is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License version 2 for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with Py-Arduino-Proxy; see the file LICENSE.txt.
+##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import os
+import sys
+
+from datetime import datetime
+
+# Setup PYTHONPATH
+SRC_DIR = os.path.split(os.path.realpath(__file__))[0] # SRC_DIR/arduino_proxy/tests
+SRC_DIR = os.path.split(SRC_DIR)[0] # SRC_DIR/arduino_proxy
+SRC_DIR = os.path.split(SRC_DIR)[0] # SRC_DIR
+sys.path.append(os.path.abspath(SRC_DIR))
+
+from arduino_proxy import ArduinoProxy, InvalidCommand, CommandTimeout, InvalidResponse
+from arduino_proxy.main_utils import default_main
+
+def main(): # pylint: disable=R0915
+    options, args, proxy = default_main() # pylint: disable=W0612
+    analog_reads = (400, 1000, )
+    try:
+        print "Initiating %d reads using analogRead()" % analog_reads[0]
+        start_analogRead = datetime.now()
+        for i in xrange(0, analog_reads[0]):
+            proxy.analogRead(0)
+        end_analogRead = datetime.now()
+        
+        print "Initiating %d reads using streamingReadAnalogPort()" % analog_reads[1]
+        start_streamingReadAnalogPort = datetime.now()
+        for i in proxy.streamingReadAnalogPort(0, analog_reads[1]):
+            pass
+        end_streamingReadAnalogPort = datetime.now()
+        
+        non_streaming = float(analog_reads[0])/(end_analogRead-start_analogRead).total_seconds()
+        streaming = float(1000.0)/(end_streamingReadAnalogPort-start_streamingReadAnalogPort).total_seconds()
+        print "analogRead() -> %f reads per second" % non_streaming
+        print "streamingReadAnalogPort() ->  %f reads per second" % streaming
+        print "speedup: X%0.2f" % (int(streaming/non_streaming))
+        
+    except KeyboardInterrupt:
+        print ""
+    except Exception:
+        raise
+    finally:
+        proxy.close()
+
+if __name__ == '__main__':
+    main()
