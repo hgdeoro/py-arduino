7c8e492f6b0a8919203915254663521962a8b623
Horacio G. de Oro <hgdeoro@gmail.com>
2012-09-21 15:49:09 -0300

    DHT11: update generate_sketch.py to include dht files

--- src/arduino_proxy/generate_sketch.py
+++ src/arduino_proxy/generate_sketch.py
@@ -152,6 +152,8 @@ def main(): # pylint: disable=R0914,R0912,R0915
     extra_source_filenames = [
         'arduino_type.h',
         'avr_cpunames.h',
+        'dht11.cpp',
+        'dht11.h',
     ]
     
     logging.info("Template for .ino file: %s", c_input_filename)
