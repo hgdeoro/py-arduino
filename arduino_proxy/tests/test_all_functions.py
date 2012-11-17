50d2ec36917c904eccad57cc50517d77605bfcfd
Horacio G. de Oro <hgdeoro@gmail.com>
2012-11-16 21:51:10 -0300

    Test of re-connecting the proxy instance

--- arduino_proxy/tests/test_all_functions.py
+++ arduino_proxy/tests/test_all_functions.py
@@ -51,7 +51,7 @@ def main(): # pylint: disable=R0915
         print "micros() -> %s" % str(proxy.micros())
         print "shiftOut() -> %s" % str(proxy.shiftOut(10, 11, ArduinoProxy.LSBFIRST, 255,
             set_pin_mode=True))
-        
+
         #define RETURN_OK 0
         #define READ_ONE_PARAM_NEW_LINE_FOUND 7
         #define READ_ONE_PARAM_EMPTY_RESPONSE 1
@@ -60,7 +60,7 @@ def main(): # pylint: disable=R0915
         #define UNEXPECTED_RESPONSE_FROM_READ_ONE_PARAM 4
         #define UNEXPECTED_RESPONSE_FROM_READ_PARAMETERS 5
         #define FUNCTION_NOT_FOUND 6
-        
+
         try:
             print "Check for READ_ONE_PARAM_ERROR_PARAMETER_TOO_LARGE"
             proxy.send_cmd("laaaarge_meeeethod_" * 10)
@@ -78,7 +78,7 @@ def main(): # pylint: disable=R0915
             # FUNCTION_NOT_FOUND == 6
             print " +", exception
             assert exception.error_code == "6"
-        
+
         try:
             print "Check for READ_PARAMETERS_ERROR_TOO_MANY_PARAMETERS"
             proxy.send_cmd("cmd\tp1\tp2\tp3\tp4\tp5\tp6\tp7\tp8\tp9\tp10")
@@ -87,25 +87,25 @@ def main(): # pylint: disable=R0915
             # READ_PARAMETERS_ERROR_TOO_MANY_PARAMETERS == 3
             print " +", exception
             assert exception.error_code == "3"
-        
+
         proxy.setTimeout(1)
         print "delay(500)"
         proxy.delay(500)
-        
+
         print "delay(2000)"
         proxy.delay(2000)
-        
+
         print "low level delay(500)"
         ret = proxy.send_cmd("%s\t500" % (proxy.delay.arduino_function_name))
         assert ret == "D_OK"
-        
+
         print "low level delay(1500)"
         try:
             proxy.send_cmd("%s\t1500" % (proxy.delay.arduino_function_name))
             assert False, "The previous line should raise an exception!"
         except CommandTimeout, exception:
             print " +", exception.__class__
-        
+
         try:
             # The INPUT buffer has the response from the timed-out delay()
             # Anything before a validateConnection() should fail.
@@ -113,37 +113,49 @@ def main(): # pylint: disable=R0915
             assert False, "The previous line should raise an exception!"
         except InvalidResponse, exception:
             pass
-        
+
         print "Re-connecting after timeout. validateConnection() -> %s" % \
             str(proxy.validateConnection())
-        
+
         # Now, the connection is valid again, ping() should work...
         print "ping() -> %s" % str(proxy.ping())
-        
+
         print "getFreeMemory() -> %s" % str(proxy.getFreeMemory())
-        
+
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Test streaming
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        
+
         # Test streaming of analog values
         for val in proxy.streamingAnalogRead(0, 10):
             print "streamingAnalogRead() -> %s" % str(val)
-        
+
         # Test streaming of analog values
         for val in proxy.streamingDigitalRead(0, 10):
             print "streamingDigitalRead() -> %s" % str(val)
-        
+
         print "validateConnection() -> %s" % str(proxy.validateConnection())
         print "millis() -> %s " % str(proxy.millis())
         # Finished!
-        
+
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Test close()/re-connect()
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        assert proxy.is_connected()
+        proxy.close()
+        assert not proxy.is_connected()
+
+        proxy.connect()
+        proxy.ping()
+
     except KeyboardInterrupt:
         print ""
     except Exception:
         raise
     finally:
-        proxy.close()
+        if proxy.is_connected():
+            proxy.close()
+
 
 if __name__ == '__main__':
     main()
