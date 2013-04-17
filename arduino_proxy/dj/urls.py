6e1ecb70af2ad356ea412f892526f639abed1a57
Horacio G. de Oro <hgdeoro@gmail.com>
2013-04-17 15:30:44 -0300

    Map /static to make it work with gunicorn

--- arduino_proxy/dj/urls.py
+++ arduino_proxy/dj/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 admin.autodiscover()
@@ -14,4 +15,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('django.contrib.staticfiles.views',
+    url(r'^static/(?P<path>.*)$', 'serve'),
 )
