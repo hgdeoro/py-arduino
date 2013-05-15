5fd0850717f5661357c4974b40f2d2d31307fae0
Horacio G. de Oro <hgdeoro@gmail.com>
2013-05-15 12:03:02 -0300

    Check env variable VIRTUAL_ENV

--- bin/run_uwsgi.sh
+++ bin/run_uwsgi.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$( cd $(dirname $0)/.. ; pwd)
 
-if [ -d $BASEDIR/virtualenv ] ; then
+if [ -z "$VIRTUAL_ENV" -a -d $BASEDIR/virtualenv ] ; then
 	. $BASEDIR/virtualenv/bin/activate
 fi
 
@@ -10,9 +10,8 @@ uwsgi \
 	--module=arduino_proxy.dj.wsgi:application \
 	--env DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-arduino_proxy.dj.settings} \
 	--master \
-	--processes=1 --enable-threads \
+	--processes=${UWSGI_PROCESSES:-1} --enable-threads \
 	--home=$BASEDIR/virtualenv \
-	--http=127.0.0.1:8080 \
+	--http=${UWSGI_HTTP:-127.0.0.1:8080} \
 	--python-path=$BASEDIR/PyArduinoProxy \
 	--static-map /static=$BASEDIR/arduino_proxy/dj/static
-
