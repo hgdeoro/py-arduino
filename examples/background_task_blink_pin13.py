1a897527696981fe4e0981983d4e38346d4fc0ab
Horacio G. de Oro <hgdeoro@gmail.com>
2013-05-23 00:36:18 -0300

    Add example of background task

---
+++ examples/background_task_blink_pin13.py
@@ -0,0 +1,39 @@
+'''
+Created on May 22, 2013
+
+@author: Horacio G. de Oro
+'''
+import Pyro4
+import hmac
+import time
+import logging
+
+from arduino_proxy.proxy import ArduinoProxy
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    Pyro4.config.HMAC_KEY = hmac.new('this-is-PyArduinoProxy').digest()
+    arduino_proxy = Pyro4.Proxy("PYRO:arduino_proxy.Proxy@localhost:61234")
+
+    logger.info("Wait some time to let PyRO server start up")
+    time.sleep(5)
+
+    logger.info("Wait until Arduino is connected...")
+    while not arduino_proxy.is_connected():
+        time.sleep(1)
+
+    logger.info("Connected! :-D")
+
+    logger.info("Starting to blink")
+    arduino_proxy.pinMode(13, ArduinoProxy.OUTPUT)
+    while True:
+        arduino_proxy.digitalWrite(13, ArduinoProxy.HIGH)
+        time.sleep(1)
+        arduino_proxy.digitalWrite(13, ArduinoProxy.LOW)
+        time.sleep(1)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
