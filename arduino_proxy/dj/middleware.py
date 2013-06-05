d9145ff27a8fa07b198e96c1ea3e20e0f89646a2
Horacio G. de Oro <hgdeoro@gmail.com>
2013-06-05 16:21:36 -0300

    Add middleware to handle ConnectionClosedError

---
+++ arduino_proxy/dj/middleware.py
@@ -0,0 +1,40 @@
+##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+##    PyArduinoProxy - Access your Arduino from Python
+##    Copyright (C) 2011-2012 - Horacio Guillermo de Oro <hgdeoro@gmail.com>
+##
+##    This file is part of PyArduinoProxy.
+##
+##    PyArduinoProxy is free software; you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation version 2.
+##
+##    PyArduinoProxy is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License version 2 for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with PyArduinoProxy; see the file LICENSE.txt.
+##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import logging
+
+from Pyro4.errors import ConnectionClosedError
+from django.http.response import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+
+
+class PyroproxyConnectionMiddleware(object):
+    """
+    Middleware that shows descriptive error on Pyro connection errors.
+    """
+    _middleware_logger = logging.getLogger('PyroproxyConnectionMiddleware')
+
+    def process_exception(self, request, exception):
+        if isinstance(exception, ConnectionClosedError):
+            self._middleware_logger.error("ConnectionClosedError detected...")
+            messages.add_message(request, messages.ERROR, "ConnectionClosedError detected")
+            return HttpResponseRedirect(reverse('home'))
+
+        return None
--- arduino_proxy/dj/settings.py
+++ arduino_proxy/dj/settings.py
@@ -105,6 +105,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'arduino_proxy.dj.middleware.PyroproxyConnectionMiddleware',
 )
 
 ROOT_URLCONF = 'arduino_proxy.dj.urls'
