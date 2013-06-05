6474af571e8362edca25c040d4815c366673893e
Horacio G. de Oro <hgdeoro@gmail.com>
2013-06-05 15:16:07 -0300

    Pyroserver: add cli and logging

--- arduino_proxy/pyroproxy/cli_ipython.py
+++ arduino_proxy/pyroproxy/cli_ipython.py
@@ -1,6 +1,6 @@
 ##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 ##    PyArduinoProxy - Access your Arduino from Python
-##    Copyright (C) 2011-2012 - Horacio Guillermo de Oro <hgdeoro@gmail.com>
+##    Copyright (C) 2011-2013 - Horacio Guillermo de Oro <hgdeoro@gmail.com>
 ##
 ##    This file is part of PyArduinoProxy.
 ##
@@ -17,30 +17,45 @@
 ##    along with PyArduinoProxy; see the file LICENSE.txt.
 ##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-import os
-import hmac
 import Pyro4
+import hmac
 
-from arduino_proxy.proxy import ArduinoProxy
+from IPython.frontend.terminal.embed import InteractiveShellEmbed
+from IPython.core.prompts import PromptManager
+
+banner = """
+
+----------------------------------------------------------------------
+PyArduinoproxy
+----------------------------------------------------------------------
+
+Launching IPython shell... Enter 'quit()' to exit.
+
+Available variables:
+    - proxy: the ArduinoProxy instance.
+
+Example:
+    >>> proxy.ping()
+    'PING_OK'
+ 
+"""
 
 
 def main():
-    """
-    Expose object using PyRO
-    """
-    if "ENABLE_LOGGING" in os.environ:
-        import logging
-        logging.basicConfig(level=logging.DEBUG)
     Pyro4.config.HMAC_KEY = hmac.new('this-is-PyArduinoProxy').digest()
-    Pyro4.config.SOCK_REUSE = True
-    proxy = ArduinoProxy()
-    Pyro4.Daemon.serveSimple(
-        {
-            proxy: "arduino_proxy.Proxy",
-            proxy.storage: "arduino_proxy.Storage",
-        },
-        host="localhost", port=61234, ns=False)
-    # FORMA DE URI -> uri_string = "PYRO:arduino_proxy.Proxy@localhost:61234"
+    proxy = Pyro4.Proxy("PYRO:arduino_proxy.Proxy@localhost:61234")
+
+    try:
+        proxy.ping()
+    except:
+        pass
+
+    PromptManager.in_template = "PyArduinoProxy [\\#]> "
+    PromptManager.out_template = "PyArduinoProxy [\\#]: "
+
+    shell = InteractiveShellEmbed(banner2=banner)
+    shell.user_ns = {}
+    shell()
 
 if __name__ == '__main__':
     main()
