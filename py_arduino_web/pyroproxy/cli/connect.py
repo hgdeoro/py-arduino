1f89a2d89878ee9dd295f8729b14466aaa616ff8
Horacio G. de Oro <hgdeoro@gmail.com>
2013-07-03 14:20:28 -0300

    Use logger instead of print

--- py_arduino_web/pyroproxy/cli/connect.py
+++ py_arduino_web/pyroproxy/cli/connect.py
@@ -17,8 +17,12 @@
 ##    along with py-arduino; see the file LICENSE.txt.
 ##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+import logging
+
 from py_arduino import DEVICE_FOR_EMULATOR
 from py_arduino_web.pyroproxy.utils import BasePyroMain
+
+logger = logging.getLogger(__name__)
 
 
 class Main(BasePyroMain):
@@ -27,14 +31,16 @@ class Main(BasePyroMain):
         if len(args) > 1:
             device = args[1]
         else:
+            logger.info("No device spcified. Will connect to ARDUINO EMULATOR")
             device = DEVICE_FOR_EMULATOR
 
-        print "Calling arduino.connect() - Connecting to {0}".format(device)
+        logger.info("Calling arduino.connect() - Connecting to %s", device)
         arduino.connect(device)
 
-        print "Calling arduino.ping() to check the connection"
+        # TODO: the ping should be optional
+        logger.info("Calling arduino.ping() to check the connection")
         ret = arduino.ping()
-        print "Ping returned: '{0}'. End.".format(ret)
+        logger.info("Ping returned: '{0}", ret)
 
 if __name__ == '__main__':
     Main().start()
