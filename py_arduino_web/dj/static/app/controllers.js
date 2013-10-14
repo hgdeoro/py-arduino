650f23b4d99ef10daa49d06e9a2d82117eb72356
Horacio G. de Oro <hgdeoro@gmail.com>
2013-10-14 01:04:00 -0300

    Rename variables

--- py_arduino_web/dj/static/app/controllers.js
+++ py_arduino_web/dj/static/app/controllers.js
@@ -9,6 +9,9 @@ var pyArduinoModule = angular.module('PyArduino', []);
 // };
 // } ]);
 
+//INPUT = 0
+//OUTPUT= 1
+
 pyArduinoModule.controller('GlobalController', function($scope) {
 
     $scope.avr_cpu_type = '(unknown)';
@@ -20,6 +23,8 @@ pyArduinoModule.controller('GlobalController', function($scope) {
 pyArduinoModule.controller('PinsController', function($scope, $http) {
 
     var get_arduino_data_url = '/angular/get_arduino_data/';
+    var INPUT = 0;
+    var OUTPUT = 1;
 
     $scope.editMode = false;
 
@@ -33,37 +38,37 @@ pyArduinoModule.controller('PinsController', function($scope, $http) {
     };
 
     $scope.pinModeDisabled = function(pinStruct) {
-        pinStruct['status.mode'] = null;
+        pinStruct.status_mode = null;
     };
 
     $scope.pinModeInput = function(pinStruct) {
-        pinStruct['status.mode'] = 'INPUT';
+        pinStruct.status_mode = INPUT;
     };
 
     $scope.pinModeOutput = function(pinStruct) {
-        pinStruct['status.mode'] = 'OUTPUT';
+        pinStruct.status_mode = OUTPUT;
     };
 
     $scope.getClassForDisabled = function(pinStruct) {
-        if(pinStruct['status.mode'] == 'INPUT' || pinStruct['status.mode'] == 'OUTPUT')
+        if(pinStruct.status_mode == INPUT || pinStruct.status_mode == OUTPUT)
             return '';
         return 'btn-primary';
     };
 
     $scope.getClassForInput = function(pinStruct) {
-        if(pinStruct['status.mode'] == 'INPUT')
+        if(pinStruct.status_mode == INPUT)
             return 'btn-primary';
         return '';
     };
 
     $scope.getClassForOutput = function(pinStruct) {
-        if(pinStruct['status.mode'] == 'OUTPUT')
+        if(pinStruct.status_mode == OUTPUT)
             return 'btn-primary';
         return '';
     };
 
-    $scope.loadPinInfo = function() {
-        console.info("loadPinInfo()");
+    $scope.refreshPinInfo = function() {
+        console.info("refreshPinInfo()");
         $http.get(get_arduino_data_url).success(function(data) {
 
             console.debug("$http.get() -> success -> " + data);
@@ -83,9 +88,9 @@ pyArduinoModule.controller('PinsController', function($scope, $http) {
             // .... .... "enabled_in_web": true,
             // .... .... "pin": 0,
             // .... .... "pin_id": null,
-            // .... .... "status.written_value": null,
-            // .... .... "status.read_value": null,
-            // .... .... "status.mode": null,
+            // .... .... "status_written_value": null,
+            // .... .... "status_read_value": null,
+            // .... .... "status_mode": null,
             // .... .... "digital": true,
             // .... .... "pk": 1,
             // .... .... "label": "Digital pin #0"
@@ -97,9 +102,9 @@ pyArduinoModule.controller('PinsController', function($scope, $http) {
             // .... "enabled_in_web": true,
             // .... "pin": 0,
             // .... "pin_id": null,
-            // .... "status.written_value": null,
-            // .... "status.read_value": null,
-            // .... "status.mode": null,
+            // .... "status_written_value": null,
+            // .... "status_read_value": null,
+            // .... "status_mode": null,
             // .... "digital": true,
             // .... "pk": 55,
             // .... "label": "Analog pin #0"
