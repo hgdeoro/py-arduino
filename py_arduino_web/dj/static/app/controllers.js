678c7b2eafd744cd5b56c0dd73cdba749bcb2bf2
Horacio G. de Oro <hgdeoro@gmail.com>
2013-10-13 21:37:28 -0300

    Use AngularJS module for PyArduino

--- py_arduino_web/dj/static/app/controllers.js
+++ py_arduino_web/dj/static/app/controllers.js
@@ -1,3 +1,7 @@
-function PinsController($scope) {
-	$scope.pins = [1, 2, 3, 4];
-}
+var pyArduinoModule = angular.module('PyArduino', []);
+
+// /angular/get_arduino_data
+
+pyArduinoModule.controller('PinsController', function($scope) {
+	$scope.pins = [ 1, 2, 3, 4 ];
+});
