8ae3d031f02299ef296f7f642a8e4d861efa3109
Horacio G. de Oro <hgdeoro@gmail.com>
2015-10-08 17:45:37 -0300

    FIX set of pythonhome

--- bin/run_uwsgi.sh
+++ bin/run_uwsgi.sh
@@ -6,12 +6,15 @@ if [ -z "$VIRTUAL_ENV" -a -d $BASEDIR/virtualenv ] ; then
 	. $BASEDIR/virtualenv/bin/activate
 fi
 
+PYTHONHOME=$(dirname $(dirname $(which python)))
+
 uwsgi \
 	--module=py_arduino_web.dj.wsgi:application \
 	--env DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-py_arduino_web.dj.settings} \
 	--master \
-	--processes=${UWSGI_PROCESSES:-1} --enable-threads \
-	--home=$BASEDIR/virtualenv \
+	--processes=${UWSGI_PROCESSES:-1} \
+	--enable-threads \
+	--home=$PYTHONHOME \
 	--http=${UWSGI_HTTP:-127.0.0.1:8080} \
 	--python-path=$BASEDIR \
 	--static-map /static=$BASEDIR/py_arduino_web/dj/static \
